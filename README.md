0ae012026681f5b6d54c32358f06714952dfbdf6
Robert Cannon <rpcann@gmail.com>
2014-08-04 14:52:38 -0400

    First cut

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 bash-files
 ==========
 
-Locations to store bash settings
+Location to store personal bash settings
+
+Setup
+-----
+`git clone git@github.com:robpc/bash-files.git ~/.bash-files`
+
+Bash
+----
+Add the following to `~/.bashrc`
+    source ~/.bash-files/bashrc
+
+Git
+---
+`git clone git@github.com:magicmonty/bash-git-prompt.git ~/.bash-git-prompt`
+
+Add the following to `~/.bashrc`
+    source ~/.bash-git-prompt/gitprompt.sh
+
+`ln -s ~/.bash-files/git-prompt-colors.sh .git-prompt-colors.sh`
+
+Add the following to `~/.gitconfig`
+    [include]
+      path = .bash-files/gitconfig
---
+++ git-prompt-colors.sh
@@ -0,0 +1,24 @@
+    GIT_PROMPT_PREFIX="["
+    GIT_PROMPT_SUFFIX="]"
+    GIT_PROMPT_SEPARATOR="|"
+    GIT_PROMPT_BRANCH="${Magenta}"
+    GIT_PROMPT_STAGED="${Red}●"
+    GIT_PROMPT_CONFLICTS="${Red}✖"
+    GIT_PROMPT_CHANGED="${Blue}✚"
+    GIT_PROMPT_REMOTE=" "
+    GIT_PROMPT_UNTRACKED="${Cyan}…"
+    GIT_PROMPT_STASHED="${BoldBlue}⚑"
+    GIT_PROMPT_CLEAN="${BoldGreen}✔"
+    GIT_PROMPT_COMMAND_OK="${Green}✔ "
+    GIT_PROMPT_COMMAND_FAIL="${Red}✘ "
+
+    GIT_PROMPT_START_USER="[${Blue}${USER}@${HOSTNAME}:${PathShort}${ResetColor}]"
+    GIT_PROMPT_START_ROOT="[${Blue}${USER}@${HOSTNAME}:${PathShort}${ResetColor}]"
+    GIT_PROMPT_END_USER=" \n${White}${Time12a}${ResetColor} $ "
+    GIT_PROMPT_END_ROOT=" \n${White}${Time12a}${ResetColor} # "
+
+    # Please do not add colors to these symbols
+    GIT_PROMPT_SYMBOLS_AHEAD="↑·"
+    GIT_PROMPT_SYMBOLS_BEHIND="↓·"
+    GIT_PROMPT_SYMBOLS_PREHASH=":"
+
